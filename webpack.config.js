51cbb382ad9cf6be98cef911ed636578b457581b
lorenries <lorenries@users.noreply.github.com>
2018-11-19 16:52:02 -0500

    install deps and add chart IDs

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = env => {
       env.deploy === "development" && new webpack.HotModuleReplacementPlugin(),
       new HtmlWebpackPlugin({
         title: "",
-        chartIDs: [],
+        chartIDs: ["viz__scatterplot", "viz__violinplot"],
         inject: false,
         template: path.resolve(__dirname, "src/index.html")
       }),
